8887c064e3f8fb38babed119caa58bc604b039df
Santiago Traversa <santiago.traversa@gmail.com>
2018-07-18 01:51:40 -0300

    ui fix

--- resources/views/partials/incident.blade.php
+++ resources/views/partials/incident.blade.php
@@ -6,18 +6,24 @@
             <a href="{{ route('dashboard.incidents.delete', ['id' => $incident->id]) }}" class="btn btn-danger confirm-action" data-method='DELETE'>{{ trans('forms.delete') }}</a>
         </div>
         @endif
+
+        @if($with_link)
+        <a href="{{ route('incident', ['id' => $incident->id]) }}" class="links">
+            <span class="label label-default">
+                <abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr>
+            </span>
+        </a>
+        @else
+            <span class="label label-default">
+                <abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr>
+            </span>
+        @endif
+
         @if($incident->component)
         <span class="label label-default">{{ $incident->component->name }}</span>
         @endif
-        <strong>{{ $incident->name }}</strong>{{ $incident->isScheduled ? trans("cachet.incidents.scheduled_at", ["timestamp" => $incident->scheduled_at_diff]) : null }}
         <br>
-        <small class="date">
-            @if($with_link)
-            <a href="{{ route('incident', ['id' => $incident->id]) }}" class="links"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr></a>
-            @else
-            <abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->timestamp_formatted }}" data-timeago="{{ $incident->timestamp_iso }}"></abbr>
-            @endif
-        </small>
+        <strong>{{ $incident->name }}</strong>{{ $incident->isScheduled ? trans("cachet.incidents.scheduled_at", ["timestamp" => $incident->scheduled_at_diff]) : null }}
     </div>
     <div class="panel-body markdown-body">
         {!! $incident->formattedMessage !!}
